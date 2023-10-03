88895315f9c0f89e6e89fb3c7a7003c6a9b8ec68
AYTEKİN KAPLAN <aoniki2018@gmail.com>
2023-10-03 22:12:44 +0300

    New Commit

--- src/main/java/com/example/example001/Example001Application.java
+++ src/main/java/com/example/example001/Example001Application.java
@@ -1,13 +1,24 @@
 package com.example.example001;
 
+import com.example.example001.People.People;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class Example001Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Example001Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Example001Application.class, args);
+    }
+
+    @GetMapping
+    public List<People> people(){
+        return List.of(new People(1L, "Aytekin Kaplan",2000,"ay@gmail.com","VAN"));
+    }
 
 }
